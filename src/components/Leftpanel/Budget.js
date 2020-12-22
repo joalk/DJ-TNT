cba49389dd2f496e1cc4d29c9fb6d8d32cd3e8ef
morgan562 <tylermor562@gmail.com>
2020-12-22 10:33:08 -0800

    working budget component
    Co-authored-by: Joal Kim <joalkims@gmail.com>

--- src/components/Leftpanel/Budget.js
+++ src/components/Leftpanel/Budget.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react'
-
 export default class Budget extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      text: '',
       budget: 0,
       expenses: []
     }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   handleChange(e) {
-    this.setState({budget:e.target.value})
+    let newText = e.target.value
+    this.setState({text: newText})
+  }
+    // why doesn't arrow functions work here?
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({budget: this.state.text})
   }
   render() {
     return (
       <div>
       <input 
         type="text"
-        value={this.state.budget}
+        // value={this.state.budget}
         onChange={this.handleChange}
         />
+        <button
+        type="submit"
+        onClick={this.handleClick}
+        >Add Budget</button>
        <h1>{`$${this.state.budget}`}</h1>
       </div>
     )
